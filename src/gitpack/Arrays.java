08ad000171a993ec4817b8eda471d9cd1cb51424
aruna <arunasree07k@gmail.com>
2024-04-29 10:16:27 +0530

    First commit....

---
+++ src/gitpack/Arrays.java
@@ -0,0 +1,12 @@
+package gitpack;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5 };
+		for (int ele : arr) {
+			System.out.println(ele);
+		}
+	}
+
+}
